cc15c93943e254b015053e94ddf583c1b51e8093
Fulvio_Vigilante <fulvio.vigilante@gmail.com>
2019-10-09 16:25:55 +0200

    Sprint 2

---
+++ main-home.js
@@ -0,0 +1,40 @@
+//! Read less and read more function in home page
+// function readMore() {
+//   var dots = document.getElementById("dots");
+//   var moreText = document.getElementById("more");
+//   var btnText = document.getElementById("myMore");
+
+//   if (dots.style.display === "none") {
+//     dots.style.display = "inline";
+//     btnText.innerHTML = "Read more";
+//     moreText.style.display = "none";
+//   } else {
+//     dots.style.display = "none";
+//     btnText.innerHTML = "Read less";
+//     moreText.style.display = "inline";
+//   }
+// }
+
+//* using addEvemtLister()
+/*
+Fetch the buttom element
+*/
+let button = document.getElementById("less");
+
+/*
+Add click event listener where we will provide logic that updates the button text
+*/
+button.addEventListener("click", function() {
+  /*
+  Update the text of the button to toggle beween "More" and "Less" when clicked
+  */
+  console.log(button.innerHTML);
+
+  if (button.innerHTML.includes("more")) {
+    button.innerHTML =
+      "less <i class='fas fa-arrow-up' data-toggle='collapse' data-target='#collapseExample' aria-expanded='false' aria-controls='collapseExample'></i>";
+  } else {
+    button.innerHTML =
+      "more <i class='fas fa-arrow-down'data-toggle='collapse' data-target='#collapseExample' aria-expanded='false' aria-controls='collapseExample'></i>";
+  }
+});
--- main-senate-house.js
+++ main-senate-house.js
@@ -42,20 +42,31 @@
 //
 //createTable(members);
 
-let members = data.results[0].members;
+//! This function use a for loop to construct a table with all the data from a stringify JSON
 
 function createTable(membersArray) {
-  var tbody = document.getElementById("tableBody");
-
   for (i = 0, len = membersArray.length; i < len; i++) {
-    var row = document.createElement("tr");
-    var fullName = document.createElement("td");
+    var tbody = document.getElementById("tableBody"); // take the id in order to know where to start
+    var row = document.createElement("tr"); // creating first element of the table
+
+    tbody.append(row); // insert row in tbody
+
+    var fullName = document.createElement("td"); // creating all other elements nested inside the first
     var party = document.createElement("td");
     var state = document.createElement("td");
     var seniority = document.createElement("td");
     var percentage = document.createElement("td");
 
-    fullName.innerHTML =
+    row.append(fullName, party, state, seniority, percentage); // insert td elements inside the row
+
+    var urlPage = document.createElement("a"); // create <a> </a> that will be nested inside fullName
+    var att = document.createAttribute("href"); // create attribute href: that will be assigned inside <a>
+
+    fullName.append(urlPage);
+    urlPage.setAttributeNode(att);
+    att.value = membersArray[i].url; // assign to href the value url http...
+
+    urlPage.innerHTML =
       membersArray[i].last_name +
       ", " +
       (membersArray[i].middle_name || " ") + // ( .. ) if true, write the content, else put blank space " "
@@ -65,10 +76,9 @@ function createTable(membersArray) {
     state.innerHTML = membersArray[i].state;
     seniority.innerHTML = membersArray[i].seniority;
     percentage.innerHTML = membersArray[i].votes_with_party_pct + " %";
-
-    tbody.append(row);
-    row.append(fullName, party, state, seniority, percentage);
   }
 }
+
+let members = data.results[0].members;
 
 createTable(members);
